519c941102ea2af81c1ea5fe932e6c848a48a3af
kuhcokrow <kuhhhckr@gmail.com>
2026-07-23 19:30:06 +0700

    fix: sharp server builder

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/api/convert": ["./node_modules/.pnpm/@img+sharp-linux-x64@*/**/*"],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "30mb",
